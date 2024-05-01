abeba880d7a0e6b81a5f9e381a5e44a229924abe
i-soj-ng <jeong4530@gmail.com>
2024-05-02 01:45:44 +0900

    Feat: reset-password 페이지 퍼블리싱

---
+++ beginner-gain-client/src/components/screen/reset-password/index.tsx
@@ -0,0 +1,48 @@
+import MainImage from "public/assets/svg/rocket-illust.svg";
+import Input from "@/components/internal/common/Input";
+import BigButton from "@/components/internal/common/BigButton";
+import { useState } from "react";
+import Header from "@/components/layout/Header";
+import useShowIllust from "@/hooks/useShowIllust";
+
+const Screen = () => {
+  const [email, setEmail] = useState<string>('');
+  const { isVisible } = useShowIllust();
+
+  return (
+    <>
+      <Header />
+      <div
+        className="flex px-[10vw] items-center"
+        style={{
+          height: 'calc(100vh - 110px)',
+        }}
+      >
+        <div className="flex w-full pb-20">
+          <div className="flex-1 flex flex-col pr-20 justify-center">
+            <h2 className="font-en text-lg font-semibold mb-[12vh]">
+              Reset Password
+            </h2>
+            <div className="mb-8">
+              <Input
+                placeholder={"이메일을 입력하세요"}
+                value={email}
+                setValue={setEmail}
+              />
+            </div>
+            <BigButton
+              name="이메일 보내기"
+              color={'purple'}
+              isFilled={true}
+            />
+          </div>
+          <div className={`w-4/12 ml-[14vw]`}>
+            <MainImage className="w-full h-full" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Screen;
--- beginner-gain-client/tailwind.config.ts
+++ beginner-gain-client/tailwind.config.ts
@@ -47,7 +47,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    backgroundSize: {
+      'screen': 'calc(100vh - 102px)',
+    }
   },
   plugins: [],
 };
+
 export default config;
