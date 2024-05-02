a73e9ade2ae09025cffaf1a3b2757b9b0ad6d7f8
jisung <cham0810@kookmin.ac.kr>
2024-05-03 01:21:38 +0900

    Feat: test 데이터로 질문기능 구현

--- beginner-gain-client/src/components/internal/make-boilerplate/ChoiceButton.tsx
+++ beginner-gain-client/src/components/internal/make-boilerplate/ChoiceButton.tsx
@@ -1,11 +1,15 @@
 interface IChoiceButton {
     order: string,
     name: string,
+    onClick: () => void,
 }
 
-const ChoiceButton = ({ order, name } : IChoiceButton) => {
+const ChoiceButton = ({ order, name, onClick } : IChoiceButton) => {
     return (
-      <div className="flex items-center gap-4 border border-purple-200 bg-purple-200/20 rounded-md p-3 w-1/2 h-14 text-purple-200 hover:bg-purple-200 hover:text-blue-300 cursor-pointer">
+      <div
+          className="flex items-center gap-4 border border-purple-200 bg-purple-200/20 rounded-md p-3 w-1/2 h-14 text-purple-200 hover:bg-purple-200 hover:text-blue-300 cursor-pointer"
+          onClick={onClick}
+      >
           <div className="flex justify-center items-center w-9 h-9 border border-purple-200 bg-gray-500 rounded-md font-en text-xs text-purple-200">
               {order}
           </div>
--- beginner-gain-client/src/components/screen/make-bolierplate/[question]/index.tsx
+++ beginner-gain-client/src/components/screen/make-bolierplate/[question]/index.tsx
@@ -4,22 +4,83 @@ import ChoiceButton from "@/components/internal/make-boilerplate/ChoiceButton";
 import ChatbotButton from "@/components/internal/make-boilerplate/ChatbotButton";
 import DarkHeader from "@/components/layout/DarkHeader";
 import BackArrow from "@/components/internal/common/BackArrow";
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
 
-const data = {
-    title: '어떤 종류의 프로젝트인가요?',
-    select: [
-        {
-            name: '웹',
-            nextQuestion: 2,
-        },
-        {
-            name: '앱',
-            nextQuestion: 3,
-        },
-    ]
-};
+interface IAnswerData {
+    name: string,
+    nextQuestion: number,
+}
+
+interface IQuestionData {
+    id: number,
+    title: string,
+    select: IAnswerData[],
+}
+
+const data : IQuestionData[] = [
+    {
+        id: 1,
+        title: '어떤 종류의 프로젝트인가요?',
+        select: [
+            {
+                name: '웹',
+                nextQuestion: 2,
+            },
+            {
+                name: '앱',
+                nextQuestion: 3,
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: '사용할 언어를 선택해주세요',
+        select: [
+            {
+                name: 'javascript',
+                nextQuestion: 4,
+            },
+            {
+                name: 'typescript',
+                nextQuestion: 4,
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: '사용할 프레임워크를 선택해주세요',
+        select: [
+            {
+                name: 'React.js',
+                nextQuestion: 5,
+            },
+            {
+                name: 'Vue.js',
+                nextQuestion: 6,
+            },
+        ]
+    },
+];
 
 const Screen = ({query} : {query: string | string[] }) => {
+    const [questionData, setQuestionData] = useState<IQuestionData>();
+
+    const router = useRouter();
+
+    const fetchQuestionData = () => {
+        const filteredData = data.filter((item) => item.id === Number(router.query.question));
+        if(filteredData.length > 0) setQuestionData(filteredData[0]);
+    }
+
+    useEffect(()=> {
+        fetchQuestionData();
+    },[router.query.question])
+
+    const handleAnswerButtonClick = (nextId : number) => {
+        router.push(`/make-boilerplate/${nextId}`);
+    }
+
     return (
         <>
           <DarkHeader />
@@ -30,16 +91,18 @@ const Screen = ({query} : {query: string | string[] }) => {
               <div className="h-[50vh] w-fit mx-auto relative mt-[6vh]">
                   <div className="absolute h-[50vh] w-fit p-28 w-full">
                       <div className="flex items-center mb-14">
-                          <p className="text-sm text-purple-200">1</p>
+                          <p className="text-sm text-purple-200">{questionData?.id}</p>
                           <PurpleArrow/>
-                          <p className="text-md text-white ml-4">{data.title}</p>
+                          <p className="text-md text-white ml-4">{questionData?.title}</p>
                       </div>
                       <div className="flex flex-col flex-wrap gap-4 h-full">
-                          {data.select.map((item, index)=>
+                          {questionData?.select.map((item, index)=>
                               <ChoiceButton
                                   key={index}
                                   order={String.fromCharCode(index + 65)} //순서를 알파벳으로 표시
-                                  name={item.name}/>
+                                  name={item.name}
+                                  onClick={() => handleAnswerButtonClick(item.nextQuestion)}
+                              />
                           )}
                       </div>
                   </div>
