ca890b97221e752348a255a67cadc6260c9f10a8
jisung <cham0810@kookmin.ac.kr>
2024-05-02 18:06:36 +0900

    Fix: font 제거

--- beginner-gain-client/src/components/layout/ComponentWrapper.tsx
+++ beginner-gain-client/src/components/layout/ComponentWrapper.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router'
-import {IBM_Plex_Sans_KR, Inter} from "next/font/google";
 import Header from '@/components/layout/Header';
-
-export const inter = Inter({
-  subsets: ["latin"],
-  display: 'swap',
-  variable: '--font-inter',
-});
-export const ibm = IBM_Plex_Sans_KR({
-  subsets: ["latin"],
-  weight: ["500","600","700"],
-  display: 'swap',
-  variable: '--font-ibm',
-});
 
 export interface IComponentWrapper {
   children: any;
@@ -24,7 +11,7 @@ const ComponentWrapper = ({ ...other }: IComponentWrapper) => {
   return (
     <>
       { router.asPath !== "/login" | "/signup" ? <Header /> : <></> }
-      <main className={`${ibm.className} ${inter.variable}`}>
+      <main>
         {other.children}
       </main>
     </>
