6dcf3c41ad808d3d9e483fac5488cc0b661248ea
i-soj-ng <jeong4530@gmail.com>
2024-05-12 00:05:38 +0900

    Feat: 페이지 리다이렉트 미들웨어 추가

---
+++ beginner-gain-client/src/middleware.ts
@@ -0,0 +1,16 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const jwt = request.cookies.get('accessId');
+
+  if (!jwt) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/make-boilerplate/:path*", "/my-boilerplate"]
+};
