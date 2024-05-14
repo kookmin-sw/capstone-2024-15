bef535763b509ffb677665e232b28006c78e7c0d
gyeongesoMin <min010724@kookmin.ac.kr>
2024-05-15 00:38:15 +0900

    fix chain.invoke

--- beginner-gain-chatbot/app/main.py
+++ beginner-gain-chatbot/app/main.py
@@ -3,19 +3,26 @@ from pydantic import BaseModel
 from chatbot import get_chain, ChainInput
 from typing import Optional
 from fastapi.requests import Request
-
+#
 app = FastAPI()
-
+#
 # Initialize the chatbot chain
 chatbot_chain = get_chain()
-
+#
+#
 @app.post("/chatbot")
 async def chat_with_bot(request: Request):
     try:
         data = await request.json()
-
-        response = chatbot_chain.invoke(data)
+        # print(data)
+        response = chatbot_chain.invoke(
+            {
+                "text": data,
+            }
+        )
 
         return {"response": response}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+########################
