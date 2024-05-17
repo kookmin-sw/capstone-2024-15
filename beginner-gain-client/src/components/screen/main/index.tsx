2b687ecded6321176f99947e81bbb396743cb664
jisung <cham0810@kookmin.ac.kr>
2024-05-17 13:04:57 +0900

    Fix: 메인이미지 변경

--- beginner-gain-client/src/components/screen/main/index.tsx
+++ beginner-gain-client/src/components/screen/main/index.tsx
@@ -4,7 +4,7 @@ import router from 'next/router';
 import MainTitle from 'public/assets/svg/beginnergain-logo-big.svg';
 import ArrowButton from "@/components/internal/common/ArrowButton";
 import Blur from 'public/assets/svg/color-blur.svg';
-import MousePointer from 'public/assets/svg/mouse-pointer.svg';
+import MainImage from 'public/assets/svg/main-image.svg';
 
 import useShowMousePointer from "@/hooks/useShowMousePointer";
 import Header from "@/components/layout/Header";
@@ -16,13 +16,13 @@ const Screen = (props) => {
     <>
       <Header isLoggedIn={props.isLoggedIn}/>
       <Blur className="fixed right-0 bottom-0 -z-50" />
-      <div className={`fixed w-1/12 right-96 bottom-80 ${isVisible ? "visible" : "invisible"}`}>
-        <MousePointer className="w-full h-full"/>
+      <div className={`absolute right-44 bottom-36 w-[36vw] ${isVisible ? "visible" : "invisible"}`}>
+        <MainImage width="100%" height="100%"/>
       </div>
       <div className="h-[calc(100vh-54px-4rem)] flex flex-col justify-center">
         <div className="ml-16 mb-10">
           <div className="w-2/5">
-            <MainTitle className="w-full h-full"/>
+            <MainTitle width="100%" height="100%"/>
           </div>
           <p className="text-sm font-medium mt-10 mb-10">
             초보개발자들을 위한<br/>
